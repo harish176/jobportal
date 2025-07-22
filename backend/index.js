a067c3b614f00df464ee9fc0e4cc4c3932905528
harish <nharishreddyn6666gmail.com>
2025-07-22 19:40:18 +0530

    commit

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ import applicationRoute from "./routes/application.route.js";
 import path from 'path';
 import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
-const _dirname = path.dirname(_filename);
+const __dirname = path.dirname(__filename);
 
 
 dotenv.config({});
