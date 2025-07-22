9cd2bb930d397eee218392225c73c208a7df10aa
harish <nharishreddyn6666gmail.com>
2025-07-22 19:02:38 +0530

    commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,3 @@
-// import path from "path"
-// import react from "@vitejs/plugin-react"
-// import { defineConfig } from "vite"
-
-// export default defineConfig({
-//   plugins: [react()],
-//   resolve: {
-//     alias: {
-//       "@": path.resolve(__dirname, "./src"),
-//     },
-//   },
-// })
-
 import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
@@ -22,15 +9,28 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  server: {
-    proxy: {
-      // Proxy requests starting with `/api` to the backend server
-      "/api": {
-        target: "https://jobportal-new-p71r.onrender.com", // replace with your backend URL
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/api/, ""), // optional: removes /api prefix
-      },
-    },
-  },
 })
+
+// import path from "path"
+// import react from "@vitejs/plugin-react"
+// import { defineConfig } from "vite"
+
+// export default defineConfig({
+//   plugins: [react()],
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./src"),
+//     },
+//   },
+//   server: {
+//     proxy: {
+//       // Proxy requests starting with `/api` to the backend server
+//       "/api": {
+//         target: "http://localhost:8000", // replace with your backend URL
+//         changeOrigin: true,
+//         secure: false,
+//         rewrite: (path) => path.replace(/^\/api/, ""), // optional: removes /api prefix
+//       },
+//     },
+//   },
+// })
