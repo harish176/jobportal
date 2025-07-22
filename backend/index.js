00173643f5258f3cee54438580477ee53b9ef722
harish <nharishreddyn6666gmail.com>
2025-07-22 19:07:53 +0530

    commit

--- backend/index.js
+++ backend/index.js
@@ -17,11 +17,11 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:'https://jobportal-new-2.onrender.com',
+    origin:'https://fluffy-salmiakki-673e86.netlify.app/',
     credentials:true
 }
 
-app.use(cors());
+app.use(cors(corsOptions));
 
 const PORT = process.env.PORT || 3000;
 
